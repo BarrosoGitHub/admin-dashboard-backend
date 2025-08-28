f6806a1f235ae2494314f4ffe223264e73dae7e1
Diogo Barroso <diogo.barroso@petrotec.com>
2025-08-28 16:12:06 +0100

    Update assembly information and regenerate binaries for OPTConfigurator

--- obj/Debug/net9.0/OPTConfigurator.AssemblyInfo.cs
+++ obj/Debug/net9.0/OPTConfigurator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OPTConfigurator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6da42c02ef3e640225e24c4987aae51fb90f71d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce4e2efb659f22d96a9b0a2de5d0b0ed3a417180")]
 [assembly: System.Reflection.AssemblyProductAttribute("OPTConfigurator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OPTConfigurator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
