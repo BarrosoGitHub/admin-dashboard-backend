2e516ad8b669cd8007ec2d5bb3ae1b736d4b60ce
Diogo Barroso <diogo.barroso@petrotec.com>
2025-09-30 09:39:32 +0100

    Add Petrotec.Opt.Data package and update project references

--- obj/Debug/net9.0/OPTConfigurator.AssemblyInfo.cs
+++ obj/Debug/net9.0/OPTConfigurator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OPTConfigurator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ea38b5ea14c349e9a9a7b947b62cbbe60e86527")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4742e68efa0c62ac7196568e903e018efc1fda5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OPTConfigurator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OPTConfigurator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
